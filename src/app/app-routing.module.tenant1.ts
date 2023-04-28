4489c7182d3e61d50fc48f554e251df60906d119
desing-solenis <DRajaram@contractor.solenis.com>
2023-04-28 20:50:16 +0530

    Commit

--- src/app/app-routing.module.tenant1.ts
+++ src/app/app-routing.module.tenant1.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TutorialDetailsComponent } from './components/tutorial-details/tutorial-details.component';
 import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
+import { TutorialsListComponent } from './components/tutorials-list/tutorials-list.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'tutorials', pathMatch: 'full' },
   { path: 'tutorials/:id', component: TutorialDetailsComponent },
-  { path: 'add', component: AddTutorialComponent }
+  { path: 'tutorials', component: TutorialsListComponent }
 ];
 
 @NgModule({
